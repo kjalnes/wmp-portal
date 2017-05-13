bf6b4c268bc27f6f2f71668514558bd5838d1ae7
kjalnes <kris.alnes@gmail.com>
2017-05-13 17:14:44 -0400

    Add globe comments

--- src/components/Globe.js
+++ src/components/Globe.js
@@ -4,8 +4,10 @@ import { rotate, animate, startRotation, stopRotation, panTo } from '../utils/gl
 class Globe extends Component {
 
     componentDidMount() {
+        // get all countries in the world to store in redux
         this.props.fetchAllCountries();
 
+        // when the component mounts, we create a new earth, otherwise rotate the existing earth
         if(!this.props.earth) {
             this.props.setEarth(new WE.map('earth_div'));
         } else {
@@ -13,24 +15,39 @@ class Globe extends Component {
         }
     }
 
+    // this method is called any time this component's state or props passed to it change
     componentWillUpdate(nextProps, nextState) {
         const currentCountry = this.props.currentCountry;
         const nextCountry = nextProps.currentCountry;
 
+        // this condition is true once the earth gets stored in redux
         if(!this.props.earth && nextProps.earth) {
             rotate(nextProps.earth);
+
+            /*
+             * when the earth is clicked, get the latitude and longitude
+             * and use the ws.geonames.org API to fetch the country code
+             * for the country clicked
+             */
             nextProps.earth.on("click", ({ latitude, longitude }) => {
+                // find any existing popups that are open
                 let previousPopups = document.querySelectorAll(".we-pp-close");
 
-                // this closes any existing popups that might be open
+                // close any existing popups that are open
                 if (previousPopups.length) {
                     previousPopups.forEach(popup => popup.click());
                 }
 
+                // get the country code based on the latitude / longitude
                 this.props.fetchCountry(latitude, longitude);
             });
         }
 
+        /*
+         * when clicking the earth, if the country clicked is not equal to the country
+         * that was previously clicked, pan to that country and show the popup with the
+         * country info
+         */
         if (currentCountry !== nextCountry && nextCountry) {
             stopRotation();
             panTo(nextProps.earth, nextProps.currentCountry.latlng);
@@ -39,6 +56,10 @@ class Globe extends Component {
         }
     }
 
+    /*
+     * this generates the HTML for the country popup
+     * (returns a string, not JSX, since that is what the popup needs)
+     */
     getPopupMarkup(country) {
         return (
             `<div>
